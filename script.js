564c06ebb4362a432e9c712d444c2d336c4cc85b
Sneha-Shaw-Itobuz <sneha@itobuz.com>
2023-02-16 10:03:53 +0530

    `fix`

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ const menu = document.getElementById('menu')
 menu.addEventListener('click',()=>{
    if(nav_list.classList.contains('hide')){
     nav_list.classList.remove('hide')
+    nav_list.classList.add('show')
    }
    else{
     nav_list.classList.add('hide')
-
+    nav_list.classList.remove('show')
    }
 })
