3e8d80efbdabf020be29c9b65e84f4039ae36237
Sneha-Shaw-Itobuz <sneha@itobuz.com>
2023-02-20 18:44:11 +0530

    `fix`

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const nav_list = document.getElementById("nav_list");
 const menu = document.getElementById("menu");
-const circle = document.querySelectorAll(".circles div");
-const cards = document.querySelectorAll(".slider-card");
-const plans_cards = document.querySelectorAll(".hover-card");
 
 menu.addEventListener("click", () => {
   if (nav_list.classList.contains("hide")) {
@@ -12,22 +9,6 @@ menu.addEventListener("click", () => {
     nav_list.classList.add("hide");
     nav_list.classList.remove("show");
   }
-});
-
-plans_cards.forEach((plan) => {
-  plan.addEventListener("mouseover", () => {
-    plan.firstElementChild.style.color = "white";
-    plan.lastElementChild.style.background = "#FAD725";
-    plan.lastElementChild.classList.remove("border-secondary");
-  });
-});
-
-plans_cards.forEach((plan) => {
-  plan.addEventListener("mouseout", () => {
-    plan.firstElementChild.style.color = "#6f77f1";
-    plan.lastElementChild.style.background = "none";
-    plan.lastElementChild.classList.add("border-secondary");
-  });
 });
 
 var swiper = new Swiper(".mySwiper", {
